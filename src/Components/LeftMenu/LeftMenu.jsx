8c5059c3f857aa6edbc30d2bb0cead66f5ece5f7
Lehito15 <266877@student.pwr.edu.pl>
2024-10-15 19:37:03 +0200

    fix: Menu with css

--- src/Components/LeftMenu/LeftMenu.jsx
+++ src/Components/LeftMenu/LeftMenu.jsx
@@ -14,101 +14,112 @@ function LeftMenu() {
         <ProfileStatistics />
       </div> 
       <div className="menu-content">
-      <nav>
-          <ul>
-            <li>
-              <NavLink 
-                exact="true" 
-                to="/" 
-                className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
-              >
-                <div className="menu-item">
-                  <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
-                  <span>Home</span>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink 
-                to="/images" 
-                className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
-              >
-                <div className="menu-item">
-                  <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
-                  <span>Trip events</span>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink 
-                to="/settings" 
-                className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
-              >
-                <div className="menu-item">
-                  <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
-                  <span>Relations</span>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink 
-                to="/rolki" 
-                className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
-              >
-                <div className="menu-item">
-                  <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
-                  <span>Groups</span>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink 
-                to="/memories" 
-                className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
-              >
-                <div className="menu-item">
-                  <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
-                  <span>Memories</span>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink 
-                to="/explore" 
-                className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
-              >
-                <div className="menu-item">
-                  <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
-                  <span>Explore</span>
-                </div>
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+        <div className='nav-container'>
+          <nav>
+              <ul>
+                <li>
+                  <NavLink 
+                    exact="true" 
+                    to="/" 
+                    className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
+                  >
+                    <div className="menu-item">
+                      <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
+                      <span className='text'>Home</span>
+                    </div>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/images" 
+                    className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
+                  >
+                    <div className="menu-item">
+                      <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
+                      <span className='text'>Trip events</span>
+                    </div>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/settings" 
+                    className={({ isActive }) => isActive ? "menu-link active" : "  menu-link"}
+                  >
+                    <div className="menu-item">
+                      <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
+                      <span className='text'>Relations</span>
+                    </div>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/rolki" 
+                    className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
+                  >
+                    <div className="menu-item">
+                      <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
+                      <span className='text'>Groups</span>
+                    </div>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/memories" 
+                    className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
+                  >
+                    <div className="menu-item">
+                      <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
+                      <span className='text'>Memories</span>
+                    </div>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/explore" 
+                    className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
+                  >
+                    <div className="menu-item">
+                      <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
+                      <span className='text'>Explore</span>
+                    </div>
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+        </div>
       </div>
+
       <div className='menu-content-down'>
-      <nav>
-          <ul>
-            <li>
-              <NavLink 
-                exact="true" 
-                to="/" 
-                className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
-              >
-                Settings
-              </NavLink>
-            </li>
-            <li>
-              <NavLink 
-                to="/images" 
-                className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
-              >
-                Logout
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>  
+        <div className='nav-container'>
+          <nav>
+              <ul>
+                <li>
+                  <NavLink 
+                    exact="true" 
+                    to="/" 
+                    className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
+                  >
+                    <div className="menu-item">
+                      <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
+                      <span className='text'>Settings</span>
+                    </div>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/images" 
+                    className={({ isActive }) => isActive ? "menu-link active" : "menu-link"}
+                  >
+                    <div className="menu-item">
+                      <img src={`${process.env.PUBLIC_URL}/relation_icon.png`} alt="Ikona" className="icon" />
+                      <span className='text'>Logout</span>
+                    </div>
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div> 
+      </div> 
     </div>
   );
 }
